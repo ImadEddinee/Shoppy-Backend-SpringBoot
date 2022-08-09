c61ba2042fe115a7ed908c6c8d60d25079184014
imad eddine <imadhajali66@gmail.com>
2022-08-09 13:07:00 +0100

    add cross origin

--- src/main/java/com/ensas/shoppybackendspring/controller/ProductController.java
+++ src/main/java/com/ensas/shoppybackendspring/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.ensas.shoppybackendspring.services.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/products")
 @RequiredArgsConstructor
+@CrossOrigin("http://localhost:4200")
 public class ProductController {
 
     private final ProductService productService;
--- src/main/java/com/ensas/shoppybackendspring/repositories/ProductRepository.java
+++ src/main/java/com/ensas/shoppybackendspring/repositories/ProductRepository.java
@@ -3,6 +3,7 @@ package com.ensas.shoppybackendspring.repositories;
 import com.ensas.shoppybackendspring.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long> {
